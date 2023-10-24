11ea3012cd789945da19587e6697a156e20095bc
HotPotatoC <juandotulung@gmail.com>
2023-10-24 22:22:59 +0700

    updated seeder

--- database/seeders/AuctionSeeder.php
+++ database/seeders/AuctionSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 
 class AuctionSeeder extends Seeder
 {
@@ -18,10 +17,10 @@ class AuctionSeeder extends Seeder
             DB::table('auctions')->insert([
                 'seller_id' => 1,
                 'winner_id' => 2,
-                'title' => Str::random(10),
-                'image_url' => 'https://majalah.ottenstatic.com/uploads/2017/07/DSC03919.jpg',
-                'description' => Str::random(20),
-                'asking_price' => rand(1, 99999),
+                'title' => fake()->sentence,
+                'image_url' => fake()->imageUrl,
+                'description' => fake()->paragraph,
+                'asking_price' => rand(1, 9999999),
                 'ends_at' => Carbon::now()->subDays(rand(1, 365)),
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
