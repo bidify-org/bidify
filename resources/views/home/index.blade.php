8ae7562b90b9501e46cd54a6c458e22d7b98a34e
HotPotatoC <juandotulung@gmail.com>
2023-10-25 00:31:31 +0700

    ga guna

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -16,21 +16,22 @@
         <ul>
             @foreach (auth()->user()->auctions as $auction)
             <div class="mb-4 mt-4 p-6 border border-gray-2 rounded-2xl">
-                <div class="flex space-x-6">
-                    <img class="max-w-sm h-full rounded-xl select-none" src="{{ $auction->image_url }}" alt="">
+                <div class="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-6">
+                    <img class="w-full lg:max-w-sm h-full rounded-xl select-none" src="{{ $auction->image_url }}"
+                        alt="">
                     <section class="flex flex-col justify-around">
-                        <h1 class="text-4xl font-bold">Title: {{ $auction->title }}</h1>
+                        <h1 class="text-xl md:text-4xl font-bold">Title: {{ $auction->title }}</h1>
                         <section>
                             <h2 class="text-lg">Seller: {{ $auction->seller->username}}</h2>
                             <h2 class="text-lg">Winner: {{ $auction->winner->username}}</h2>
                         </section>
 
                         <section>
-                            <p class="text-primary-blue text-xl">{{ $auction->description }}</p>
+                            <p class="text-primary-blue text-lg md:text-xl">{{ $auction->description }}</p>
                             <p class="text-green-600 text-lg">Starts at: @money($auction->asking_price)</p>
                         </section>
 
-                        <div class="flex items-center space-x-4">
+                        <div class="flex flex-col mt-4 md:mt-0 md:flex-row md:items-center md:space-x-4">
                             <section>
                                 <span>Ends at: </span>
                                 <span class="text-red-600">{{ $auction->ends_at }}</span>
