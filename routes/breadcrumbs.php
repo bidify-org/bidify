b6353946dd341556bdd6a3b99681210ace2e78bb
HotPotatoC <juandotulung@gmail.com>
2023-11-28 16:33:15 +0700

    fix(breadcrumbs): wrong home route

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -11,7 +11,7 @@ use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
 
 // Home
 Breadcrumbs::for('home', function (BreadcrumbTrail $trail) {
-    $trail->push('Home', route('auctions.store'));
+    $trail->push('Home', route('home'));
 });
 
 // Home > Blog
