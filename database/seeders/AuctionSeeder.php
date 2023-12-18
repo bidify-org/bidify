050fd944ea8b1e1b17201f645113318b8148cb75
HotPotatoC <juandotulung@gmail.com>
2023-12-19 01:22:10 +0700

    feat: added placeholder images in storage

--- database/seeders/AuctionSeeder.php
+++ database/seeders/AuctionSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Storage;
 
 class AuctionSeeder extends Seeder
 {
@@ -13,13 +14,21 @@ class AuctionSeeder extends Seeder
      */
     public function run(): void
     {
+        $placeholderImages = [
+            "public/images/placeholder-1.png",
+            "public/images/placeholder-2.png",
+            "public/images/placeholder-3.png",
+            "public/images/placeholder-4.png",
+            "public/images/placeholder-5.png",
+        ];
+
         for ($i = 0; $i < 50; $i++) {
             $askingPrice = rand(1, 9999999);
             DB::table('auctions')->insert([
                 'seller_id' => 1,
                 'winner_id' => 2,
                 'title' => fake()->sentence,
-                'image_url' => fake()->imageUrl,
+                'image_url' => $placeholderImages[rand(0, 4)],
                 'description' => fake()->paragraph,
                 'asking_price' => $askingPrice,
                 'buy_now_price' => $askingPrice * 2,
